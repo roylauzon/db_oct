fe636824562ec7f874c24e7a48c4b358e3920c41
roylauzon <email>
2021-10-05 14:26:20 +0000

    init

---
+++ dbo/Procedures/Creation-Scripts/Proc_1.sql
@@ -0,0 +1,15 @@
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[Proc_1]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	-- test comment
+
+END
